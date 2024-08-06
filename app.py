5a91a4e259b9cf926c94898baee205533dc651a5
helloPKOK <hello.pkok@protonmail.com>
2024-08-06 06:56:40 +0545

    Update app.py

--- app.py
+++ app.py
@@ -253,4 +253,4 @@ def edit_userprofile():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
